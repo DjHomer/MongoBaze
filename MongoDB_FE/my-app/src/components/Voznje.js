77767268d0de1bbc097496965f1631195270a9d0
Luka Radojkovic <lukarad@elfak.rs>
2022-02-09 14:21:48 +0100

    male izmene oko fe

--- MongoDB_FE/my-app/src/components/Voznje.js
+++ MongoDB_FE/my-app/src/components/Voznje.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
+import Spinner from './Spinner';
+import Api from '../services/Api.js'
 
 function Voznje(){
+
+    
+
     return (
         <div>
             <h1>Voznje pisi </h1>
