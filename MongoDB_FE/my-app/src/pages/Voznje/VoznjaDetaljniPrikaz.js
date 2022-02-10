34437f6cdebe14bc1876a7e2aabc31798f9e8158
IvySan <ivanapetkovic@elfak.rs>
2022-02-10 20:55:08 +0100

    neki detalji

--- MongoDB_FE/my-app/src/pages/Voznje/VoznjaDetaljniPrikaz.js
+++ MongoDB_FE/my-app/src/pages/Voznje/VoznjaDetaljniPrikaz.js
@@ -11,51 +11,44 @@ function VoznjaDetaljniPrikaz() {
 
     const {id}=useParams();
 
-    const {data:l, loading:load1, error:err1}=Api("Voznja/VratiVoznju/"+id);
-    
-    let pom="000000000000000000000000";
-    if(l!==null)
-        pom=l.busPreduzece;
+    const {data:v, loading:load1, error:err1}=Api("Voznja/VratiVoznju/"+id);
 
     if(err1) throw err1;
     if(load1) return <Spinner/>;
 
-    console.log(l);
-   // console.log(busPreduzece);
-   //let navigate = useNavigate(); <h5>Naziv:{busPreduzece.Naziv}</h5> 
-   
-
-    console.log(moment().isBefore(l.datumVoznje));
+    console.log(v);
+  
+    console.log(moment().isBefore(v.datumVoznje));
     return (
         <div className={"form"}  style={{textAlign:"left"}}>
             <div className={"naslov"}  style={{width:"100%"}}>
             <h2>Pogledajte detaljne informacije o vožnji</h2> 
             </div>
             <div className={"slova"} style={{width:"100%"}}>
-            <h3>Polazni grad: {l.polazniGrad}</h3> </div>
+            <h3>Polazni grad: {v.polazniGrad}</h3> </div>
             <div className={"slova"} style={{width:"100%"}}>
-            <h3>Dolazni grad: {l.dolazniGrad}</h3> </div>
+            <h3>Dolazni grad: {v.dolazniGrad}</h3> </div>
             <div className={"slova"} style={{width:"100%"}}>
-            <h3>Datum: {l.datumVoznje}</h3> 
+            <h3>Datum: {v.datumVoznje}</h3> 
             </div>
             <div className={"slova"} style={{width:"100%"}}>
-            <h3>Tip vožnje: {l.tipVoznje}</h3> 
+            <h3>Tip vožnje: {v.tipVoznje}</h3> 
             </div>
             <div className={"slova"} style={{width:"100%"}}>
-            <h3>Cena vožnje: {l.cenaVoznje}</h3> 
+            <h3>Cena vožnje: {v.cenaVoznje}</h3> 
             </div>
             <div>
 
             </div>
             
             <div className='dugmici' style={{width:"100%"}}>
-                {l.busPreduzece!=="" && <BusPreduzeceNaziv id={l.busPreduzece}/>}
+                {v.busPreduzece!=="" && <BusPreduzeceNaziv id={v.busPreduzece}/>}
                 
                 <div class="float-child" style={{width:"250%"}}>
-                {l.busPreduzece!=="" && <Link to={`/busPreduzeca/${l.busPreduzece}`} className="button1">Više o preduzeću</Link>} 
+                {v.busPreduzece!=="" && <Link to={`/busPreduzeca/${v.busPreduzece}`} className="button1">Više o preduzeću</Link>} 
                 </div>
                 <div class="float-child" style={{width:"25%"}}>
-                {moment().isBefore(l.datumVoznje) && <Link to={`/KreirajPutnika/${l.id}`} className="button1">Rezerviši</Link>} </div>    
+                {moment().isBefore(v.datumVoznje) && <Link to={`/KreirajPutnika/${v.id}`} className="button1">Rezerviši</Link>} </div>    
             </div>
         </div>
     )
